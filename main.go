232bf739d8daeab3a7a0fcc1aee51890516c1492
Ivo van der Wijk <github@in.m3r.nl>
2018-02-04 11:11:57 +0100

    reformat block defs; make sure all rows equally long

--- main.go
+++ main.go
@@ -27,13 +27,33 @@ type Block struct {
 	color raylib.Color
 }
 
-var square = Block{cells: []string{"XX", "XX"}, color: raylib.Red}
-var line = Block{cells: []string{"XXXX"}, color: raylib.Green}
-var leftL = Block{cells: []string{"X", "XXX"}, color: raylib.Blue}
-var rightL = Block{cells: []string{"  X", "XXX"}, color: raylib.Orange}
-var z = Block{cells: []string{"XX", " XX"}, color: raylib.Yellow}
-var reverseZ = Block{cells: []string{" XX", "XX"}, color: raylib.Gray}
-var triangle = Block{cells: []string{" X", "XXX"}, color: raylib.Purple}
+var square = Block{cells: []string{
+	"XX",
+	"XX",
+}, color: raylib.Red}
+var line = Block{cells: []string{
+	"XXXX",
+}, color: raylib.Green}
+var leftL = Block{cells: []string{
+	"X  ",
+	"XXX",
+}, color: raylib.Blue}
+var rightL = Block{cells: []string{
+	"  X",
+	"XXX",
+}, color: raylib.Orange}
+var z = Block{cells: []string{
+	"XX ",
+	" XX",
+}, color: raylib.Yellow}
+var reverseZ = Block{cells: []string{
+	" XX",
+	"XX ",
+}, color: raylib.Gray}
+var triangle = Block{cells: []string{
+	" X ",
+	"XXX",
+}, color: raylib.Purple}
 
 var blocks = []Block{square, line, leftL, rightL, z, reverseZ, triangle}
 
